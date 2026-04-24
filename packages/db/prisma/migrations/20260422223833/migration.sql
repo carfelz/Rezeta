48e73496ba93dc750fa1d2be67a47566bf318f83
Carlos Feliz <carlosjfeliz7@gmail.com>
2026-04-24 18:06:38 -0400

    chore: adding required elements to build pipeline -- fixing migration

--- packages/db/prisma/migrations/20260422223833/migration.sql
+++ packages/db/prisma/migrations/20260422223833/migration.sql
@@ -1,14 +1,5 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[tenant_id,name]` on the table `protocol_types` will be added. If there are existing duplicate values, this will fail.
-
-*/
 -- DropForeignKey
 ALTER TABLE "protocol_templates" DROP CONSTRAINT "protocol_templates_tenant_id_fkey";
-
--- CreateIndex
-CREATE UNIQUE INDEX "protocol_types_tenant_id_name_key" ON "protocol_types"("tenant_id", "name");
 
 -- AddForeignKey
 ALTER TABLE "protocol_templates" ADD CONSTRAINT "protocol_templates_tenant_id_fkey" FOREIGN KEY ("tenant_id") REFERENCES "tenants"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
